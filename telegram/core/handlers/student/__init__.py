83b94be59ff30bcf3fdfc2c1145bdb600ee47454
Alexander <izmailov.alexisss@gmail.com>
2022-07-14 23:48:23 +0300

    Implemented a course selection algorithm Problems: 1) There are no accesses to the database 2) Buttons are removed when editing messages

--- telegram/core/handlers/student/__init__.py
+++ telegram/core/handlers/student/__init__.py
@@ -2,7 +2,7 @@
 from aiogram import Dispatcher
 from loguru import logger
 from . import courses, personal_lessons, personal_schedule, basket
-
+from core.filters.guest_filters import CallBackFilter
 
 def setup(dp: Dispatcher):
     "Setups handlers for student."
@@ -16,4 +16,10 @@ def setup(dp: Dispatcher):
                                 regexp="Мое рассписание", state="student_main")
     dp.register_message_handler(basket.get_basket, regexp="Корзина",
                                 state="student_main")
+    dp.register_callback_query_handler(courses.callback_detail,CallBackFilter("detail"),
+                               state="student_main")
+    dp.register_callback_query_handler(courses.callback_standard,CallBackFilter("standard"),
+                                       state="student_main")
+    dp.register_callback_query_handler(courses.callback_pro,CallBackFilter("pro"),
+                                       state="student_main")
     logger.debug("End student's handlers registration.")
--- telegram/core/keyboards/all_keyboards.py
+++ telegram/core/keyboards/all_keyboards.py
@@ -1,6 +1,8 @@
 from aiogram.types import (
     ReplyKeyboardMarkup,
     KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
 )
 
 all_keyboards = {}
@@ -16,5 +18,27 @@ def kb_student_menu():
     )
     return kb_student
 
+def ib_student_tarifselect_1():
+    kb_student = InlineKeyboardMarkup(row_width=2)
+    kb_student.add(
+        InlineKeyboardButton(text="Добавить стандарт",callback_data="standard"),
+        InlineKeyboardButton(text="Добавить ПРО",callback_data="pro"),
+        InlineKeyboardButton(text="Подробнее", callback_data="detail")
+    )
+    return kb_student
+def ib_student_tarifselect_2():
+    kb_student = InlineKeyboardMarkup(row_width=2)
+    kb_student.add(
+        InlineKeyboardButton(text="Добавить стандарт",callback_data="standard"),
+        InlineKeyboardButton(text="Добавить ПРО",callback_data="pro"),
+    )
+    return kb_student
+def ib_student_detail():
+    kb_student = InlineKeyboardMarkup()
+    kb_student.add(InlineKeyboardButton(text="Подробнее", callback_data="detail"))
+    return kb_student
 
 all_keyboards["student_menu"] = kb_student_menu
+all_keyboards["student_tarifselect_1"] = ib_student_tarifselect_1
+all_keyboards["student_detail"] = ib_student_detail
+all_keyboards["student_tarifselect_2"] = ib_student_tarifselect_2
