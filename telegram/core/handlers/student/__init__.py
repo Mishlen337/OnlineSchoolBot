31581eade47c8bc72352b3ba4632e3ff7599c56c
Alexander <izmailov.alexisss@gmail.com>
2022-07-21 19:31:29 +0300

    Implemented a payment mechanism Problems: 1) Payment sometimes takes place with incorrect card data (perhaps this is due to the test mode) 2) When a payment fails due to incorrect card data, a repeat payment with correct data similarly fails (perhaps this is due to the test mode)

--- telegram/core/handlers/student/__init__.py
+++ telegram/core/handlers/student/__init__.py
@@ -1,5 +1,6 @@
 """Package to declare student's handlers."""
 from aiogram import Dispatcher
+from aiogram import types
 from loguru import logger
 from . import courses, personal_lessons, personal_schedule, basket
 from core.filters.guest_filters import CallBackFilter
@@ -9,12 +10,13 @@ def setup(dp: Dispatcher):
     logger.debug("Start student's handlers registration.")
     dp.register_message_handler(courses.get_courses, regexp="Выбрать курсы",
                                 state="student_main")
-    dp.register_message_handler(personal_lessons.get_lessons,
-                                regexp="Выбрать индивидуальные занятия",
+    dp.register_message_handler(personal_lessons.get_lessons, regexp="Выбрать индивидуальные занятия",
                                 state="student_main")
-    dp.register_message_handler(personal_schedule.get_schedule,
-                                regexp="Мое рассписание", state="student_main")
+    dp.register_message_handler(personal_schedule.get_schedule, regexp="Мое рассписание",
+                                state="student_main")
     dp.register_message_handler(basket.get_basket, regexp="Корзина",
+                                state="student_main")
+    dp.register_message_handler(basket.clear_basket, regexp="Очистить корзину",
                                 state="student_main")
     dp.register_callback_query_handler(courses.callback_detail,CallBackFilter("detail"),
                                state="student_main")
@@ -22,4 +24,8 @@ def setup(dp: Dispatcher):
                                        state="student_main")
     dp.register_callback_query_handler(courses.callback_pro,CallBackFilter("pro"),
                                        state="student_main")
+    dp.register_pre_checkout_query_handler(basket.checkout_process,
+                                           state="student_main")
+    dp.register_message_handler(basket.successful_payment, content_types=types.ContentType.SUCCESSFUL_PAYMENT,
+                                state="student_main")
     logger.debug("End student's handlers registration.")
--- telegram/core/keyboards/all_keyboards.py
+++ telegram/core/keyboards/all_keyboards.py
@@ -7,7 +7,6 @@ from aiogram.types import (
 
 all_keyboards = {}
 
-
 def kb_student_menu():
     kb_student = ReplyKeyboardMarkup(resize_keyboard=True)
     kb_student.add(
@@ -26,6 +25,7 @@ def ib_student_tarifselect_1():
         InlineKeyboardButton(text="Подробнее", callback_data="detail")
     )
     return kb_student
+
 def ib_student_tarifselect_2():
     kb_student = InlineKeyboardMarkup(row_width=2)
     kb_student.add(
@@ -33,12 +33,24 @@ def ib_student_tarifselect_2():
         InlineKeyboardButton(text="Добавить ПРО",callback_data="pro"),
     )
     return kb_student
+
 def ib_student_detail():
     kb_student = InlineKeyboardMarkup()
     kb_student.add(InlineKeyboardButton(text="Подробнее", callback_data="detail"))
+    return kb_student
+
+def kb_student_menubasket():
+    kb_student = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb_student.add(
+        KeyboardButton("Выбрать курсы"),
+        KeyboardButton("Выбрать индивидуальные занятия"),
+        KeyboardButton("Мое рассписание"),
+        KeyboardButton("Очистить корзину")
+    )
     return kb_student
 
 all_keyboards["student_menu"] = kb_student_menu
 all_keyboards["student_tarifselect_1"] = ib_student_tarifselect_1
 all_keyboards["student_detail"] = ib_student_detail
 all_keyboards["student_tarifselect_2"] = ib_student_tarifselect_2
+all_keyboards["student_menubasket"] = kb_student_menubasket
