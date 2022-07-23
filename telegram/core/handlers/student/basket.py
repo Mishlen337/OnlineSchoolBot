ba27032f678329ac4770bf33957bcc577be20a4b
Alexander <izmailov.alexisss@gmail.com>
2022-07-23 14:48:46 +0300

    Implemented the output of a single message with payment for all selected courses without discount Problems: 1) The description of the message is not formatted in any way

--- telegram/core/handlers/student/basket.py
+++ telegram/core/handlers/student/basket.py
@@ -5,25 +5,25 @@ from aiogram.dispatcher.storage import FSMContext
 from core.keyboards.all_keyboards import all_keyboards
 from core.config import bot, dp, config
 
-mathematics = {'id_course': 0,
+mathematics = { 'id_course': 0,
         'name_course':'Математика подготовка к ЕГЭ',
         'name_subject':'Математика',
         'name_teacher':'Галаган Гомункулич Васильевич',
-        'price_course':'1000 рублей',
+        'price_course':'100 рублей',
         'Basket': True,
         'Selected': False}
 physics = {'id_course': 1,
         'name_course':'Физика подготовка к ЕГЭ',
         'name_subject':'Физика',
         'name_teacher':'Калинина биг босс абудаби',
-        'price_course':'1000 рублей',
+        'price_course':'100 рублей',
         'Basket': True,
         'Selected': False}
 informatics = {'id_course': 2,
         'name_course':'Информатика подготовка к ЕГЭ',
         'name_subject':'Информатика',
         'name_teacher':'Измайлов Марат Айратович',
-        'price_course':'1000 рублей',
+        'price_course':'100 рублей',
         'Basket': True,
         'Selected': False}
 Subjects = [mathematics,physics,informatics]
@@ -35,20 +35,21 @@ async def get_basket(message: types.Message, state: FSMContext):
     num_of_courses = len(Subjects)
     if num_of_courses > 0:
         await message.answer(f"Курсы в корзине: ",parse_mode='HTML', reply_markup=all_keyboards["student_menubasket"]())
+        PRICES = []
+        Info_course = ''
         for i in range(num_of_courses):
             Sub = Subjects[i]
             price = int(Sub['price_course'].split(" ")[0])*100
-            PRICES = [types.LabeledPrice(label=Sub['name_course'], amount=price)]
-            Info_course = (f"{Sub['name_subject']}\n"
-                           f"{Sub['name_teacher']}\n")
-            await bot.send_invoice(message.chat.id,
-                                   title=Sub['name_course'],
-                                   description=Info_course,
-                                   provider_token=config.PAYMENTS_SECRET,
-                                   currency='rub',
-                                   prices=PRICES,
-                                   payload=Sub['name_subject'],
-                                   start_parameter='basket')
+            PRICES.append(types.LabeledPrice(label=Sub['name_course'], amount=price))
+            Info_course += (f"{Sub['name_subject']}\n{Sub['name_teacher']}\n\n")
+        await bot.send_invoice(message.chat.id,
+                               title='Выбранные курсы',
+                               description=Info_course,
+                               provider_token=config.PAYMENTS_SECRET,
+                               currency='rub',
+                               prices=PRICES,
+                               payload='pay-course-basket',
+                               start_parameter='basket')
     else:
         await message.answer("Корзина пуста.")
 
