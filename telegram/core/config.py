2e91bc857d6a4cea838399baead7ad24dd26bb37
Mikhail Isakov <maisakov@miem.hse.ru>
2022-09-06 03:39:51 +0300

    add: redis

--- telegram/core/config.py
+++ telegram/core/config.py
@@ -1,6 +1,7 @@
 """Module to declare basic variables."""
 from aiogram import Dispatcher, Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from pydantic import BaseSettings
 
 
@@ -13,12 +14,17 @@ class Settings(BaseSettings):
     MODERATOR_TG_ID: int
     TERMS_PATH: str
 
+    REDIS_HOST: str
+    REDIS_PORT: str
+    REDIS_DB: int
+    REDIS_PASSWORD: str
+
     class Config:
         env_file = "../.env"
 
 
 config = Settings()
 
-storage = MemoryStorage()
+storage = RedisStorage2(config.REDIS_HOST, config.REDIS_PORT, config.REDIS_DB, config.REDIS_PASSWORD)
 bot = Bot(token=config.TELEGRAM_SECRET)
 dp = Dispatcher(bot, storage=storage)
